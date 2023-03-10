c86377d38b03bb36074afbef44fe34cbc838bba8
Ashley Le <53335012+lediemhang87@users.noreply.github.com>
2023-03-10 01:27:29 -0800

    Doctor dashboard

--- halodoc/src/Components/DoctorDashboard.js
+++ halodoc/src/Components/DoctorDashboard.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect}  from 'react';
 import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import '../Styles/Styles.Dashboard.scss'
+import '../Styles/Styles.DoctorDashboard.scss'
+import DoctorNavbar from './DoctorNavbar'
 
 const DoctorDashboard = () => {
     let navigate = useNavigate();
@@ -10,20 +12,7 @@ const DoctorDashboard = () => {
     useEffect(() => {
       axios.get('/doctorDashboard').then(function (response) {
       setBackendData(response.data)
-    })})
-
-
-    const handleLogout= async (e) => {
-      e.preventDefault();
-      console.log('Click logout')
-      try {
-        const response = await axios.post('/doctorlogout');
-        console.log(response.data.data)
-        navigate(response.data.data)
-      } catch (err) {
-        console.error(err);
-      }
-    };
+    })}, [])
 
     return(
         <div>
@@ -33,10 +22,106 @@ const DoctorDashboard = () => {
                   <img src={require("../Styles/img/loading.gif")} alt="" width="300px"/> 
                 </div>
             ) : (
-              <div >
-                <h1> {backendData.data.firstName} {backendData.data.lastName} </h1> 
-                <button onClick={handleLogout}> Log out </button>
+              
+              <div className='DoctorDashboard' >
+                <DoctorNavbar name= {backendData.data.firstName + backendData.data.lastName} />
+                <div className='container'>
+                  <div className='row'>
+                    <div className='col-lg-2'>
+                     
+                    </div>
+                    <div className='col-lg-8'>
 
+                      {/* Greeting box (Good morning...) */}
+                      <div className='Greetings'>
+                        <div className='row'>
+                          <div className='col-sm-9'>
+                            <div className='greet-text'>
+                            <h1> Good morning, <br/> Dr {backendData.data.firstName} {backendData.data.lastName}</h1>
+                            <p> Have a nice day! </p>
+                            </div>
+                          </div>
+                          <div className='col-sm-3'>
+                            <img src={require("../Styles/img/doctor.png")} alt='' width='100%'></img>
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* All features that doctor can redirect to - contains: all schedule, all patients, and edit info */}
+                      <div className='DoctorDashboardInfo'>
+                        <h2> Information </h2>
+                        <div className='container'>
+                          <div className='row'>
+                            <div className='col'>
+                              <button className="btn btn-light " onClick={() => navigate('/docschedule')}>  See your schedule 
+                                <img src={require("../Styles/img/calendar_icon.png")} width='100%'/> 
+                              </button> 
+                              
+                            </div>
+
+                            <div className='col'> 
+                                <button className="btn btn-light "> 
+                                
+                                  Show all patients 
+                                  <img src={require("../Styles/img/patient.png") } width='100%'/>
+                                  
+                                </button>
+                            </div>
+
+                            <div className='col'> 
+                              <button className="btn btn-light "> Edit your information 
+                                <img src={require("../Styles/img/information.png") } width='100%'/>
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+
+                        </div>
+
+                        {/* Today appointment */}
+                        <div className='TodayAppointment'>
+                          <h2> Today's appointment </h2>
+                          <div className='container'>
+
+                            <div class="card header">
+                              <div class="card-body">
+                                <div className='row'> 
+                                  <div className='col-sm-3'> Patient name </div>
+                                  <div className='col-sm-2'> Start time </div>
+                                  <div className='col-sm-4'> Patient email </div>
+                                  <div className='col-sm-3'> Patient birthday</div>
+                                </div>
+                              </div>
+                            </div>
+                        
+                            {backendData.appointment
+                              .sort((a, b) => a.startTime > b.startTime ? 1 : -1)
+                              .map(appt => (
+                              <div class="card">
+                                <div class="card-body">
+                                  <div className='row'> 
+                                    <div className='col-sm-3'> {appt.patientName} </div>
+                                    <div className='col-sm-2'> {appt.startTime} </div>
+                                    <div className='col-sm-4'> {appt.patientEmail}</div>
+                                    <div className='col-sm-3'> {appt.patientBirthday}</div>
+                                  </div>
+                                  
+                                </div>
+                              </div>
+                            ))}
+                    
+                        </div>
+                      </div>
+                    </div>
+
+
+                      
+
+                    <div className='col-lg-2'> 
+                      .
+                    </div>
+                  </div>
+                </div>
               </div>  
             )}
         </div>
